971907a6a5511b9fc2e5c62616bed95c23d27e96
Gopal Kataria <gopalkataria@outlook.com>
2020-09-24 12:06:19 +0530

    initail commit with cs clas codes :

---
+++ cs1.c
@@ -0,0 +1,12 @@
+// sample program from CS class
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name: Gopal Kataria\n");
+    printf("\tDate: 22 September 2020\n");
+    printf("Day: Tuesday\n");
+
+    return 0;
+}
