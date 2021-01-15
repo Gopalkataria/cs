1d5935743d656a61e7cd8b878e402d953706e288
Gopal Kataria <gopalkataria@outlook.com>
2021-01-15 08:32:46 +0530

    added matrix multiplication code

--- theory/multiplicationOfMatrices.c
+++ theory/multiplicationOfMatrices.c
@@ -5,39 +5,65 @@
 
 int main()
 {
+    int rowSizeA, colSizeA, rowSizeB, colSizeB, row, col;
 
-    int rowSize, colSize, row, col;
-    printf(" Enter the row and column size of the matrices ");
-    scanf("%d%d", &rowSize, &colSize);
-
-    int matrixA[rowSize][colSize], matrixB[rowSize][colSize], sumMatrixAB[rowSize][colSize];
-
+    // taking input for Matrix A
+    printf(" Enter the row and column size of the matrix A ");
+    scanf("%d%d", &rowSizeA, &colSizeA);
+    int matrixA[rowSizeA][colSizeA];
     printf("Enter elements of Matrix A\n");
-    for (int row = 0; row < rowSize; row++)
+    for (int row = 0; row < rowSizeA; row++)
     {
         printf("Row %d : ", row + 1);
-        for (col = 0; col < colSize; col++)
+        for (col = 0; col < colSizeA; col++)
         {
-            scanf("%d", &matrixA[row][col]); /// taking input for elemets of matrix A
+            scanf("%d", &matrixA[row][col]);
         }
     }
 
+    // taking input for elements of Matrix B
+    printf(" Enter the row and column size of the matrix B ");
+    scanf("%d%d", &rowSizeB, &colSizeB);
+    int matrixB[rowSizeB][colSizeB];
     printf("Enter elements of Matrix B \n");
-    for (row = 0; row < rowSize; row++)
+    for (row = 0; row < rowSizeB; row++)
     {
         printf("Row %d : ", row + 1);
-        for (col = 0; col < colSize; col++)
+        for (col = 0; col < colSizeB; col++)
         {
-            scanf("%d", &matrixB[row][col]);                               // taking input for elements of matrix B
-            sumMatrixAB[row][col] = matrixB[row][col] + matrixA[row][col]; // adding the elements right away
+            scanf("%d", &matrixB[row][col]);
+        }
+    }
+
+    // input validation
+    if (colSizeA != rowSizeB)
+    {
+        printf(" Matrix multiplication is not possible between the inputted Matrices");
+        return 0;
+    }
+
+    // logic for multiplying the matrices
+    int productMatrixAB[rowSizeA][colSizeB]; // the matrix to hold the product
+    for (row = 0; row < rowSizeA; row++)
+    {
+        for (col = 0; col < colSizeB; col++)
+        {
+            productMatrixAB[row][col] = 0; // setting initial value to 0
+            for (int cell = 0; cell < rowSizeB; cell++)
+            {
+                // traverse through matrixA horizontally, thus matrixA[row][cell]
+                // traverse through matrixB vertically, thus matrixB[cell][col]
+                // and adding the product to the corresponding cell in productMatrix
+                productMatrixAB[row][col] += matrixA[row][cell] * matrixB[cell][col];
+            }
         }
     }
 
     // printing elements of matrix A
     printf("\n\nMatrix A\n");
-    for (row = 0; row < rowSize; row++)
+    for (row = 0; row < rowSizeA; row++)
     {
-        for (col = 0; col < colSize; col++)
+        for (col = 0; col < colSizeA; col++)
         {
             printf("%d ", matrixA[row][col]);
         }
@@ -46,22 +72,22 @@ int main()
 
     // printing elements of matrix B
     printf("\n\nMatrix B\n");
-    for (row = 0; row < rowSize; row++)
+    for (row = 0; row < rowSizeB; row++)
     {
-        for (col = 0; col < colSize; col++)
+        for (col = 0; col < colSizeB; col++)
         {
             printf("%d ", matrixB[row][col]);
         }
         printf("\n");
     }
 
-    // printing elements of matrix A + B
-    printf("\n\nMatrix A + Matrix B\n");
-    for (row = 0; row < rowSize; row++)
+    // printing elements of matrix AB
+    printf("\n\n product of Matrix A and Matrix B\n");
+    for (row = 0; row < rowSizeA; row++)
     {
-        for (col = 0; col < colSize; col++)
+        for (col = 0; col < colSizeB; col++)
         {
-            printf("%d ", sumMatrixAB[row][col]);
+            printf("%d ", productMatrixAB[row][col]);
         }
         printf("\n");
     }
